18b4f2184c62facb86652f6c1eed2ca8add0928d
masteroksi <masteroksi@gmail.com>
2020-06-08 20:30:39 +0300

    lesson02

---
+++ lesson02-advanced.js
@@ -0,0 +1,33 @@
+'use strict';
+const num = 266219;
+
+const multiplicationOfNumbers = (number) => {
+    return number
+        .toString()
+        .split('')
+        .reduce((result, number) => {
+            return result * number;
+        }, 1);
+};
+
+console.log(multiplicationOfNumbers(num));
+
+const multiplicationOfNumbers2 = (number) => {
+    const numberStr = number.toString();
+    const numberCount = numberStr.length;
+    let i = 0;
+    let result = 1;
+    while (i < numberCount) {
+        result *= Number(numberStr.charAt(i));
+        i++;
+    }
+    return result;
+};
+
+console.log(multiplicationOfNumbers2(num));
+
+function pow(num, exponent) {
+    return num ** exponent;
+}
+
+console.log(pow(multiplicationOfNumbers(num), 3));
